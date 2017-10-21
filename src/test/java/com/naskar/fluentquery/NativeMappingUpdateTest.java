74d3b4d5721cb8ac6e47e0fdfb86a5b5371f672b
rafaeluchoa <rafael@naskar.com.br>
2017-10-21 18:59:07 -0300

    more tests

--- src/test/java/com/naskar/fluentquery/NativeMappingUpdateTest.java
+++ src/test/java/com/naskar/fluentquery/NativeMappingUpdateTest.java
@@ -20,7 +20,7 @@ public class NativeMappingUpdateTest {
 	}
 	
 	@Test
-	public void testInsert() {
+	public void testUpdate() {
 		String expected = "update TB_CUSTOMER e0 set e0.DS_NAME = :p0, e0.VL_MIN_BALANCE = :p1 where e0.CD_CUSTOMER = :p2";
 		
 		NativeSQLResult result = new UpdateBuilder()
@@ -37,6 +37,24 @@ public class NativeMappingUpdateTest {
 		Assert.assertEquals(result.params().get("p0"), "teste");
 		Assert.assertEquals(result.params().get("p1"), 10.2);
 		Assert.assertEquals(result.params().get("p2"), 1L);
+	}
+	
+	@Test
+	public void testUpdateWithoutWhere() {
+		String expected = "update TB_CUSTOMER e0 set e0.DS_NAME = :p0, e0.VL_MIN_BALANCE = :p1";
+		
+		NativeSQLResult result = new UpdateBuilder()
+			.entity(Customer.class)
+				.value(i -> i.getName()).set("teste")
+				.value(i -> i.getMinBalance()).set(10.2)
+			.to(new NativeSQLUpdate(mc));
+		
+		String actual = result.sql();
+		
+		Assert.assertEquals(expected, actual);
+		
+		Assert.assertEquals(result.params().get("p0"), "teste");
+		Assert.assertEquals(result.params().get("p1"), 10.2);
 	}
 	
 }
--- src/test/java/com/naskar/fluentquery/NativeUpdateTest.java
+++ src/test/java/com/naskar/fluentquery/NativeUpdateTest.java
@@ -10,7 +10,7 @@ import com.naskar.fluentquery.domain.Customer;
 public class NativeUpdateTest {
 	
 	@Test
-	public void testInsert() {
+	public void testUpdate() {
 		String expected = "update Customer e0 set e0.name = :p0, e0.minBalance = :p1 where e0.id = :p2";
 		
 		NativeSQLResult result = new UpdateBuilder()
@@ -27,6 +27,24 @@ public class NativeUpdateTest {
 		Assert.assertEquals(result.params().get("p0"), "teste");
 		Assert.assertEquals(result.params().get("p1"), 10.2);
 		Assert.assertEquals(result.params().get("p2"), 1L);
+	}
+	
+	@Test
+	public void testUpdateWithoutWhere() {
+		String expected = "update Customer e0 set e0.name = :p0, e0.minBalance = :p1";
+		
+		NativeSQLResult result = new UpdateBuilder()
+			.entity(Customer.class)
+				.value(i -> i.getName()).set("teste")
+				.value(i -> i.getMinBalance()).set(10.2)
+			.to(new NativeSQLUpdate());
+		
+		String actual = result.sql();
+		
+		Assert.assertEquals(expected, actual);
+		
+		Assert.assertEquals(result.params().get("p0"), "teste");
+		Assert.assertEquals(result.params().get("p1"), 10.2);
 	}
 	
 }
